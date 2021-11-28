16ced8c372f7c38dd3e53d798e993f449085fd28
Cao Duy Lâm <duylamasd@gmail.com>
2021-11-28 22:33:39 +0700

    add server env

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig, UserConfigExport } from "vite";
+import { defineConfig, UserConfigExport, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const config: UserConfigExport = {
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: /^@mui\/icons-material\/(.*)/,
-        replacement: "@mui/icons-material/esm/$1",
-      },
-    ],
-  },
+const config: UserConfigExport = ({ mode }) => {
+  const appEnv = loadEnv(mode, process.cwd(), "SERVER_");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: /^@mui\/icons-material\/(.*)/,
+          replacement: "@mui/icons-material/esm/$1",
+        },
+      ],
+    },
+    envPrefix: "APP_",
+    server: {
+      port: parseInt(appEnv.SERVER_PORT || "3000"),
+    },
+  };
 };
 
 export default defineConfig(config);
